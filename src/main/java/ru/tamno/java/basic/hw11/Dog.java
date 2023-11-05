b78dda7cdcb8011c82e2eb012903914237140d1e
Yana <yana@MacBook-Pro-Yana.local>
2023-11-06 02:08:26 +0300

    Исправлены все замечания

--- src/main/java/ru/tamno/java/basic/hw11/Dog.java
+++ src/main/java/ru/tamno/java/basic/hw11/Dog.java
@@ -5,4 +5,17 @@ public class Dog extends Animal {
         super(name, speedRun, speedSwim, endurance);
         this.swimmingEnduranceCost = 2;
     }
+
+    @Override
+    public float swim(int distance) {
+        float timeSwim = 0;
+        timeSwim = distance / speedSwim;
+        System.out.println("Затрачено " + timeSwim + " минут");
+        endurance -= distance * swimmingEnduranceCost;
+        if (endurance <= 0) {
+            System.out.println("У " + name + " появилось состояние усталости");
+            return -1;
+        }
+        return timeSwim;
+    }
 }
