e1312fd5f6291cf8ff410345b9b76a0a5e881eb9
Stbran <70580989+stbran@users.noreply.github.com>
2023-11-25 01:54:59 +0300

    Внесение изменений в соответствии с замечаниями в PR

--- src/main/java/ru/tamno/java/basic/hw14/Main.java
+++ src/main/java/ru/tamno/java/basic/hw14/Main.java
@@ -4,10 +4,11 @@ package ru.tamno.java.basic.hw14;
 public class Main {
     public static void main(String[] args) {
         String[][] arr = new String[][]{{"0","0","0","0"},{"0","0","0","0"},{"0","0","0","0"}};
+//        String[][] arr = new String[][]{{"0","a","0","0"},{"0","0","0","0"},{"0","0","0","0"},{"0","0","0"}};
 //        String[][] arr = new String[][]{{"0","a","0","0"},{"0","0","0","0"},{"0","0","0","0"},{"0","0","0","0"}};
-//        String[][] arr = new String[][]{{"1","1","1","1"},{"1","1","1","1"},{"1","1","1","1"},{"1","1","1","1"}};
+//        String[][] arr = new String[][]{{"1", "1", "1", "1"}, {"1", "1", "1", "1"}, {"1", "1", "1", "1"}, {"1", "1", "1", "1"}};
         try {
-            sumElementsArray(arr);
+            System.out.println("Сумма элементов массива = " + sumElementsArray(arr));
         } catch (AppArraySizeException e) {
             System.out.println(e.getMessage());
         } catch (AppArrayDataException e) {
@@ -15,20 +16,22 @@ public class Main {
         }
     }
 
-    public static void sumElementsArray(String arr[][]) throws AppArraySizeException, AppArrayDataException {
-        if (arr.length != 4 || arr[0].length != 4) {
-            throw new AppArraySizeException("Неверный размер массива");
-        }
-        int sumElements = 0;
-        String regex = "\\d+";
+    public static int sumElementsArray(String arr[][]) throws AppArraySizeException {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (!arr[i][j].matches(regex)) {
-                    throw new AppArrayDataException("Error in arr[" + i + "][" + j + "]");
-                }
-                sumElements += Integer.valueOf(arr[i][j]);
+            if (arr.length != 4 || arr[i].length != 4) {
+                throw new AppArraySizeException("Неверный размер массива");
             }
         }
-        System.out.println("Сумма элементов массива = " + sumElements);
+        int sumElements = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    sumElements += Integer.valueOf(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new AppArrayDataException("Error in arr[" + i + "][" + j + "]");
+                }
+            }
+        }
+        return sumElements;
     }
 }
