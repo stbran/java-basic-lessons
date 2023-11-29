46e7fe18209042f4959dc8436f1b361dc741763a
Stbran <mistral-ya@yandex.ru>
2023-11-30 02:02:01 +0300

    change Phonebook

--- src/main/java/ru/tamno/java/basic/lesson16/PhoneBook.java
+++ src/main/java/ru/tamno/java/basic/lesson16/PhoneBook.java
@@ -1,41 +1,46 @@
 package ru.tamno.java.basic.lesson16;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class PhoneBook {
     private Map<String, String> phoneBook = new HashMap<>();
-    private Set<Map.Entry<String, String>> entries = phoneBook.entrySet();
 
     public void add(String name, String phone) {
         phoneBook.put(phone, name);
     }
 
-    public void find(String name) {
+    public Set<String> find(String name) {
         int count = 0;
-        for (Map.Entry<String, String> entry : entries) {
+        Set<String> phones = new HashSet<>();
+        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
             if (entry.getValue().equals(name)) {
-                System.out.println(entry.getValue() + " " + entry.getKey());
+                phones.add(entry.getKey());
+                System.out.println("Для " + name + " сохранен номер " + entry.getKey());
                 count++;
             }
         }
         if (count == 0) {
             System.out.println("В базе нет человека с именем " + name);
         }
+        return phones;
     }
 
-    public void containsPhoneNumber(String phone) {
+    public boolean containsPhoneNumber(String phone) {
         if (phoneBook.containsKey(phone)) {
             System.out.println("Номер " + phone + " найден");
+            return true;
         } else {
             System.out.println("Номер " + phone + " не найден");
         }
+        return false;
     }
 
     public void print() {
         int i = 1;
-        for (Map.Entry<String, String> entry : entries) {
+        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
             System.out.println(i++ + ". " + entry.getValue() + ": " + entry.getKey());
         }
     }
