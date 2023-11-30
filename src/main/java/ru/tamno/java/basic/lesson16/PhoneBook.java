5a67d1c644d16aeb313b53c0ff5434c6cef5c089
Stbran <mistral-ya@yandex.ru>
2023-12-01 02:01:31 +0300

    Изменена структура ключ-значение

--- src/main/java/ru/tamno/java/basic/lesson16/PhoneBook.java
+++ src/main/java/ru/tamno/java/basic/lesson16/PhoneBook.java
@@ -1,47 +1,43 @@
 package ru.tamno.java.basic.lesson16;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class PhoneBook {
-    private Map<String, String> phoneBook = new HashMap<>();
+    private Map<String, Set<String>> phoneBook = new TreeMap<>();
 
     public void add(String name, String phone) {
-        phoneBook.put(phone, name);
+        if (phoneBook.containsKey(name)) {
+            phoneBook.get(name).add(phone);
+        } else {
+            phoneBook.put(name, new LinkedHashSet<>());
+            phoneBook.get(name).add(phone);
+        }
     }
 
     public Set<String> find(String name) {
-        int count = 0;
-        Set<String> phones = new HashSet<>();
-        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
-            if (entry.getValue().equals(name)) {
-                phones.add(entry.getKey());
-                System.out.println("Для " + name + " сохранен номер " + entry.getKey());
-                count++;
-            }
-        }
-        if (count == 0) {
+        if (!phoneBook.containsKey(name)) {
             System.out.println("В базе нет человека с именем " + name);
+            return null;
         }
-        return phones;
+        System.out.println("Для " + name + " найдена запись: " + phoneBook.get(name));
+        return phoneBook.get(name);
     }
 
     public boolean containsPhoneNumber(String phone) {
-        if (phoneBook.containsKey(phone)) {
-            System.out.println("Номер " + phone + " найден");
-            return true;
-        } else {
-            System.out.println("Номер " + phone + " не найден");
+        for (Map.Entry<String, Set<String>> entry : phoneBook.entrySet()) {
+            if (entry.getValue().contains(phone)) {
+                System.out.println("Номер " + phone + " найден. Принадлежит " + entry.getKey());
+                return true;
+            }
         }
+        System.out.println("Номер " + phone + " не найден");
         return false;
     }
 
     public void print() {
         int i = 1;
-        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
-            System.out.println(i++ + ". " + entry.getValue() + ": " + entry.getKey());
+        for (String key : phoneBook.keySet()) {
+            System.out.println(i++ + ". " + key + ": " + phoneBook.get(key));
         }
     }
 }
